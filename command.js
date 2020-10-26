a9172fa307609d83d05b5af7f89bce0d1c51f267
Benjamin Nack <ben@nackfamily.com>
2020-10-26 15:38:48 -0500

    Add command.js to register commands and add a command for the lenny face

---
+++ command.js
@@ -0,0 +1,36 @@
+let commands = [];
+
+let cmdtk = '!';
+
+module.exports = {
+    registerCommandToken: (token = '!') => {
+        cmdtk = token
+    },
+
+    registerCommand: (cmd = "", argcount, action, description = "", usage = "") => {
+        commands.push({action, cmd, argcount, description, usage});
+    },
+
+    executeCommands: (message) => {
+        if(message.content.charAt(0) === cmdtk){
+            commands.forEach((cmd) => {
+
+                if(message.content.split(" ")[0].split("!")[1].toLowerCase() !== cmd.cmd)
+                    return;
+
+                let args = message.content.split(" ").splice(0, 1);
+
+                if(args.length > cmd.argcount){
+                    message.reply(`You gave to many arguments. Usage: ${cmd.usage}`);
+                    return;
+                }
+                if(args.length < cmd.argcount){
+                    message.reply(`This command requires more arguments. Usage: ${cmd.usage}`);
+                    return;
+                }
+
+                cmd.action(message, args);
+            });
+        }
+    }
+}
--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+let CMD = require('./command');
+
+/* Commands */
+
+/* ASCII Characters */
+
+CMD.registerCommand('lenny', 0, (msg, args) => { msg.channel.send('( ͡° ͜ʖ ͡°)') });
+
+/* Bot Code */
+
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
 });
+
+client.on('message', (message) => CMD.executeCommands(message));
 
 client.login(process.env.DISCORD_BOT_TOKEN);
