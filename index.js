cd8fbd034c4a426740b261659776353a00551df9
Benjamin Nack <ben@nackfamily.com>
2020-10-27 10:57:54 -0500

    Added crayola and partyart

--- index.js
+++ index.js
@@ -18,6 +18,9 @@ function sendChar(char, msg){
 
 CMD.registerCommand('lenny', 0, (msg, args) => sendChar('( ͡° ͜ʖ ͡°)', msg), "Displays ( ͡° ͜ʖ ͡°)", 'lenny');
 CMD.registerCommand('shrug', 0, (msg, args) => sendChar('¯\\_(ツ)_/¯', msg), "Displays ¯\\_(ツ)_/¯", 'shrug');
+CMD.registerCommand('partyart', 0, (msg, args) => sendChar('♫♪.ılılıll|̲̅̅●̲̅̅|̲̅̅=̲̅̅|̲̅̅●̲̅̅|llılılı.♫♪¯', msg), "Displays ♫♪.ılılıll|̲̅̅●̲̅̅|̲̅̅=̲̅̅|̲̅̅●̲̅̅|llılılı.♫♪", 'partyart');
+CMD.registerCommand('crayola', 0, (msg, args) => sendChar('((̲̅ ̲̅(̲̅C̲̅r̲̅a̲̅y̲̅o̲̅l̲̲̅a̲̅( ̲̅((>', msg), "Displays ((̲̅ ̲̅(̲̅C̲̅r̲̅a̲̅y̲̅o̲̅l̲̲̅a̲̅( ̲̅((>", 'crayola');
+
 
 /* Bot Code */
 
